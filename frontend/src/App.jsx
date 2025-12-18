91c38f792187003366867ae7cd2cf9b821573814
Mashego Mabeloane <mabeloanegideon1000@gmail.com>
2025-12-18 07:16:33 +0200

    firebase connected with app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-console.log("🚀 App rendered");
-import "./firebase";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
-console.log("🚀 App rendered");
-  return <h1>Hello</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello</h1>;
 }
 
 export default App
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 
-console.log("🔥 Firebase initialized", app);
+console.log("Firebase initialized", app);
 
 
 
